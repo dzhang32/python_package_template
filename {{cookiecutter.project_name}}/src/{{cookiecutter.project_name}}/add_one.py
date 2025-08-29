895578ae39375652a0d94f626614bc758fa1f876
David Zhang <32676710+dzhang32@users.noreply.github.com>
2025-08-29 15:10:26 +0100

    build: add test data dir

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/add_one.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/add_one.py
@@ -1,3 +1,20 @@
+from pathlib import Path
+
+
+def add_one_file(input_file: Path) -> list[int]:
+    """
+    Example function that adds one to the input.
+
+    Args:
+        input_file: The input file containing one integer per line.
+
+    Returns:
+        A list of integers, one for each line in the input file.
+    """
+    with open(input_file, "r") as f:
+        return [add_one(int(line.strip())) for line in f]
+
+
 def add_one(x: int) -> int:
     """
     Example function that adds one to the input.
--- {{cookiecutter.project_name}}/tests/test_add_one.py
+++ {{cookiecutter.project_name}}/tests/test_add_one.py
@@ -1,7 +1,13 @@
-from {{cookiecutter.project_name}}.add_one import add_one
+from {{cookiecutter.project_name}}.add_one import add_one, add_one_file
+from pathlib import Path
 
+def test_add_one_file(test_data_dir: Path) -> None:
+    """
+    Test that the add_one_file function works correctly.
+    """
+    assert add_one_file(test_data_dir / "test_add_one" / "integers.txt") == [2, 3, 4]
 
-def test_add_one():
+def test_add_one() -> None:
     """
     Test that the add_one function works correctly.
     """
